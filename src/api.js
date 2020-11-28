9934cdd8af54c1d003dd7e44d98d2d9125e9ca8e
mwConceptX <mwieja@yahoo.com>
2020-11-28 21:16:58 +0100

    set api

---
+++ src/api.js
@@ -0,0 +1,28 @@
+// base url
+
+const base_url = "https://api.rawg.io/api/";
+
+//date
+
+let today = new Date();
+let dd = today.getDate();
+
+let mm = today.getMonth() + 1;
+const yyyy = today.getFullYear();
+if (dd < 10) {
+  dd = `0${dd}`;
+}
+
+if (mm < 10) {
+  mm = `0${mm}`;
+}
+const currentYear = `${yyyy}-${mm}-${dd}`;
+const lastYear = `${yyyy - 1}-${mm}-${dd}`;
+const nextYear = `${yyyy + 1}-${mm}-${dd}`;
+
+//popular
+
+export default const popularGamesUrl = () =>
+  `${base_url}games?dates=${lastYear},${currentYear}&ordering=-rating&page_size=10`;
+
+
